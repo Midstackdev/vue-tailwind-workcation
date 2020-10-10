f0cc48244f413e2d6007ed507c816b51f024280d
Midstackdev <midstackdev@gmail.com>
2020-10-10 12:24:04 +0000

    Mobile

---
+++ tailwind.config.js
@@ -0,0 +1,63 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {},
+    customForms: theme => ({
+      default: {
+        input: {
+          borderRadius: theme('borderRadius.lg'),
+          backgroundColor: theme('colors.gray.700'),
+          '&:focus': {
+            backgroundColor: theme('colors.white'),
+          }
+        },
+        select: {
+          borderRadius: theme('borderRadius.lg'),
+          backgroundColor: theme('colors.gray.700'),
+          '&:focus': {
+            backgroundColor: theme('colors.gray.600'),
+            borderColor: 'transparent',
+            boxShadow: 'none'
+          },
+          borderColor: 'transparent',
+          boxShadow: theme('boxShadow.default'),
+          color: theme('colors.white'),
+          lineHeight: theme('lineHeight.snug'),
+          icon: '<svg fill="#fff" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>',
+        },
+        checkbox: {
+          width: theme('spacing.6'),
+          height: theme('spacing.6'),
+          backgroundColor: theme('colors.gray.900'),
+          '&:focus': {
+            backgroundColor: theme('colors.gray.700'),
+            borderColor: 'transparent',
+            boxShadow: 'none'
+          },
+          borderColor: 'transparent',
+          checkedColor: theme('colors.indigo.500'),
+        },
+        radio: {
+          width: theme('spacing.6'),
+          height: theme('spacing.6'),
+          backgroundColor: theme('colors.gray.900'),
+          '&:focus': {
+            backgroundColor: theme('colors.gray.700'),
+            borderColor: 'transparent',
+            boxShadow: 'none'
+          },
+          borderColor: 'transparent',
+          checkedColor: theme('colors.indigo.500'),
+        }
+      },
+    })
+  },
+  variants: {},
+  plugins: [
+    require('@tailwindcss/custom-forms')
+  ],
+}
